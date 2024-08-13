34fef2dba4f3ef853ba02227b478d9e0c9ed9481
Dura07 <Oyedejioluwasheye2013@gmail.com>
2024-08-12 18:00:55 -0700

    Star Wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,39 +1,26 @@
+
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const movieUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
 
-if (!movieId) {
-  console.error('Please provide a movie ID as an argument.');
-  process.exit(1);
-}
-
-request(movieUrl, function (error, response, body) {
-  if (error || response.statusCode !== 200) {
-    console.error('Error fetching movie data:', error || `Status Code: ${response.statusCode}`);
-    return;
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  const characters = JSON.parse(body).characters;
-  if (!characters || characters.length === 0) {
-    console.error('No characters found for the given movie.');
-    return;
-  }
-
-  printCharactersSequentially(characters, 0);
 });
 
-function printCharactersSequentially(characters, index) {
-  if (index >= characters.length) {
-    return;
-  }
-
+function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+    if (!error) {
       console.log(JSON.parse(body).name);
-      printCharactersSequentially(characters, index + 1);
-    } else {
-      console.error('Error fetching character data:', error || `Status Code: ${response.statusCode}`);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
     }
   });
 }
